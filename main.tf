efd7e630a8c51d2f79d1a5fff291c776143114ce
simran-smartbear <144090183+simran-smartbear@users.noreply.github.com>
2023-09-07 15:33:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,24 +2,8 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = ami-0f5ee92e2d63afc18
   instance_type = var.instance_type
 
   tags = {
