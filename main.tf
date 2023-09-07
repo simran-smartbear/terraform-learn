7aa49fba6b2dba695de9e71aa7933f27bc3697b2
simran-smartbear <144090183+simran-smartbear@users.noreply.github.com>
2023-09-07 15:35:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = ami-0f5ee92e2d63afc18
+  ami           = "ami-0f5ee92e2d63afc18"
   instance_type = var.instance_type
 
   tags = {
