afa269e3009d1799705ec11a9dad1bf31bb5fd22
simran-smartbear <144090183+simran-smartbear@users.noreply.github.com>
2023-11-02 17:43:16 +0530

    Revert "Update variables.tf" (#4)

--- variables.tf
+++ variables.tf
@@ -5,10 +5,10 @@ variable "region" {
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+  default     = "t3.medium"
 }
 
 variable "instance_name" {
   description = "EC2 instance name"
-  default     = "test-instance"
+  default     = "Provisioned by Terraform"
 }
